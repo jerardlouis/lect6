4453649195a0662498031c70917876cff382b785
Jerard Dayanghirang <jerardlguevarra@gmail.com>
2021-02-09 18:18:01 +0000

    Updated dates

--- app.py
+++ app.py
@@ -10,14 +10,14 @@ app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 def hello_world():
     """ Returns root endpoint HTML """
 
-    keyword_query = 'Gamestop' # Change it to something you're interested in!
+    keyword_query = 'Dogecoin' # Change it to something you're interested in!
     article_data = get_article_data(keyword_query)
-
     return render_template(
         "index.html",
         topic=keyword_query,
         headlines=article_data['headlines'],
         snippets=article_data['snippets'],
+        dates=article_data['dates']
     )
 
 app.run(
